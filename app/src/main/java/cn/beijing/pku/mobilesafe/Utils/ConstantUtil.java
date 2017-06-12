dbbd8e8d27ad845d8db7fab84ed441a99585b689
joli <zhangenwei64@163.com>
2017-06-12 14:04:21 +0800

    6.12

---
+++ app/src/main/java/cn/beijing/pku/mobilesafe/Utils/ConstantUtil.java
@@ -0,0 +1,9 @@
+package cn.beijing.pku.mobilesafe.Utils;
+
+/**
+ * Created by Administrator on 2017.6.2.
+ */
+public class ConstantUtil {
+//    Splash界面显示的版本名称
+    public static final String VERSION_NAME = "版本名称" ;
+}
